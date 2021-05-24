e487031c13efbcddf5b04a128e1cf5390c2116dc
Nick Martello <thenickmartello@gmail.com>
2021-05-24 18:30:44 -0400

    sound function added

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -91,3 +91,22 @@ document
 document
   .querySelector('.game--restart')
   .addEventListener('click', handleRestartGame);
+
+$('.cell').click(() => {
+  if ($('.glow').length) {
+    $('#light').removeClass('glow');
+    pauseSound('buzz')
+  } else {
+    $("#light").addClass('glow');
+    playSound('buzz')
+  }
+})
+
+
+function playSound(animal) {
+  document.getElementById(animal).play();
+};
+
+function pauseSound(animal) {
+  document.getElementById(animal).pause();
+};
